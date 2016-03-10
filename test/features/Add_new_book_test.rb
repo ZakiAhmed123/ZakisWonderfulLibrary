14b9fb8fa3f59936e39adbedde720053b9c2ce36
“Zaki <reach.zaki.ahmed@gmail.com>
2016-03-10 16:24:55 -0600

    newbooktest

--- test/features/Add_new_book_test.rb
+++ test/features/Add_new_book_test.rb
@@ -2,6 +2,9 @@ require "test_helper"
 
 class AddNewBook < Capybara::Rails::TestCase
 
+setup do
+  Author.create! first_name: "Malcolm", last_name: "X", bio: "YYYYYY", photo_id:"http://www.ultimatestock.co.uk/wp-content/uploads/2016/02/Modern-Romance-Written-By-Aziz-Ansari-500x600.jpg"
+end
 test "Can Add New Book" do
 
   visit root_path
@@ -9,9 +12,10 @@ test "Can Add New Book" do
   click_link "New Book"
   within("#new_book") do
   fill_in "Title", with: "MODERN ROMANCE"
-  fill_in "Price", with: "$17"
+  fill_in "Price", with: "$33"
   fill_in "Photo", with: "http://www.ultimatestock.co.uk/wp-content/uploads/2016/02/Modern-Romance-Written-By-Aziz-Ansari-500x600.jpg"
-  select "Malcolm X", :from => "Author"
+  save_and_open_page
+  select("Malcolm X", from: "Author")
   click_button "Create Book"
 
   assert_content page, "MODERN ROMANCE"
